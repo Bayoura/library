9ac4759f59ef74a7efdd0167b29683c9661cee99
Bayoura <Kristin.Seidelmann@gmx.de>
2021-10-20 17:09:52 +0200

    add data-index attribute and add book info in p tags

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const author_input = document.getElementById('author');
 const totalPages_input = document.getElementById('page-number');
 const readStatus_input = document.getElementById('read-status');
 const submitForm_button = document.querySelector('button[type="submit"]');
+let rmButton = '';
 
 let myLibrary = [
     {
@@ -32,6 +33,9 @@ function showForm() {
 document.addEventListener('DOMContentLoaded', () => {
     submitForm_button.addEventListener('click', e => addBookToLibrary(e));
 })
+//you can submit a book without filling all fields! the required attribute doesn't work 
+//because we prevented the normal submit. also, the evenListener triggers on a click, not   
+//on a submit
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -47,26 +51,43 @@ function addBookToLibrary(e) {
     let pages = totalPages_input.value;
     let read = readStatus_input.checked;
     let currentBook = new Book(title,author,pages,read);
+    inputForm_form.reset();
     myLibrary.push(currentBook);
     appendToContainer(currentBook);
 }
 
 function appendToContainer(currentBook) {
-    const book = document.createElement('div');
-    container_div.append(book);
-        for (let prop in currentBook) {
-            book.textContent += currentBook[prop];
-        }
+    const bookCard = document.createElement('div');
+    rmButton = document.createElement('button');
+    rmButton.classList.add('rm-btn');
+    rmButton.textContent = 'Remove';
+    bookCard.setAttribute('data-index', (myLibrary.length -1));
+    bookCard.classList.add('book-card');
+    container_div.append(bookCard);
+    for (let prop in currentBook) {
+        const item = document.createElement('p');
+        item.textContent += currentBook[prop];
+        bookCard.append(item);
+    }
+    bookCard.append(rmButton);
 }
 
 //append all books in array to container:
 //
 // function appendToContainer() {
 //     for(let i = 0; i < myLibrary.length; i++) {
-//         const book = document.createElement('div');
-//         container_div.append(book);
+//         const bookCard = document.createElement('div');
+//         const rmButton = document.createElement('button');
+//         rmButton.classList.add('rm-btn');
+//         rmButton.textContent = 'Remove';
+//         bookCard.setAttribute('data-index', (myLibrary.length -1));
+//         bookCard.classList.add('book-card');
+//         container_div.append(bookCard);
 //         for (let prop in myLibrary[i]) {
-//             book.textContent += myLibrary[i][prop];
+//             const item = document.createElement('p');
+//             item.textContent += myLibrary[i][prop];
+//             bookCard.append(item);
 //         }
+//         bookCard.append(rmButton);
 //     }
 // }
